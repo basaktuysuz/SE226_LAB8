ad28f5924615c1dd3abb96a808e22a64b501ab3a
Başak Tüysüz <85128377+basaktuysuz@users.noreply.github.com>
2022-06-19 00:50:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,46 @@ class Base(ABC):
             print(f'line {count}: {line}')
 
 class listCount(Base):
+    def __init__(self, address):
+        Abstract.__init__(self, address)
+
     def calculateFreqs(self):
-        pass
+        strange = open(self.address, 'r').read()
+        strange = strange.split()
+        strange2 = []
+
+        for i in strange:
+            if i not in strange2:
+                strange2.append(i)
+
+        for i in range(0, len(strange2)):
+            print(strange2[i], '=', strange.count(strange2[i]))
 
 
-class DictCount(Base):
+class DictCount(Abstract):
+    def __init__(self, address):
+        Abstract.__init__(self, address)
+
     def calculateFreqs(self):
-        pass
+        strange = open(self.address, 'r').read()
+
+        dictionary = {}
+
+        lst = strange.split()
+        for elements in lst:
+            dictionary[elements]  = 0
+       
+        for elements in lst:
+            if elements[-1] == '.':
+                elements = elements[0:len(elements) - 1]
+
+            if elements in dictionary:
+                dictionary[elements] += 1
+
+            else:
+                dictionary.update({elements: 1})
+
+        for allKeys in dictionary:
+            print("\"", allKeys, "\"", end=" ")
+            print(dictionary[allKeys], end=" ")
+            print()
